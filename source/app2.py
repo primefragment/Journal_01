3837c6273b4a9b28cd82364cfa8e63bb6d373d2b
vshah <115954895+primefragment@users.noreply.github.com>
2024-10-22 13:07:31 -0400

    Formatting to make the charts look better.

--- source/app2.py
+++ source/app2.py
@@ -13,14 +13,13 @@ df = pd.read_csv(root_dir / 'data' / 'daily_journal_01.csv')
 
 # Figures
 fig1=px.line(df, x='Date', y='Wakeup Time', title='Wakeup Time', line_shape='spline', range_y=[4, 9], width=300)
-fig1.update_layout(xaxis = dict(tickformat='%d-%b\n%a'))
-
+fig1.update_layout(xaxis = dict(tickformat='%d-%b\n%a', title_text = ''), yaxis = dict(title_text = ''))
 
 fig2=px.line(df, x='Date', y=['Hours Planned','Hours Worked'], title='Hours Planned/Worked',  line_shape='spline', width=300)
-fig2.update_layout(legend_title='', xaxis = dict(tickformat='%d-%b\n%a'))
+fig2.update_layout(legend_title='', xaxis = dict(tickformat='%d-%b\n%a', title_text = ''), yaxis = dict(tickmode='linear',title_text = ''))
 
-fig3=px.bar(df, x='Date', y=['GameDev','Growth','Trading','Venture'], title='Hours Breakdown', width=300)
-fig3.update_layout(legend_title='', bargap=0.5, xaxis= dict(tickformat='%d-%b\n%a'))
+fig3=px.bar(df, x='Date', y=['GameDev','Growth','Trading','Venture'], title='Work Breakdown', width=300)
+fig3.update_layout(legend_title='', bargap=0.5, xaxis= dict(tickformat='%d-%b\n%a', tickmode='array', tickvals=df['Date'], title_text = ''), yaxis = dict(title_text = ''))
 
 
 # App and Layout
