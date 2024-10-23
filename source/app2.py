eee2ee1bd9e8e161e74d4c68933db0dff012e61f
vshah <115954895+primefragment@users.noreply.github.com>
2024-10-23 07:30:02 -0400

    Wake up time added.

--- source/app2.py
+++ source/app2.py
@@ -15,7 +15,7 @@ df = pd.read_csv(root_dir / 'data' / 'daily_journal_01.csv')
 fig1=px.line(df, x='Date', y='Wakeup Time', title='Wakeup Time', line_shape='spline', range_y=[4,9], width=300)
 fig1.update_layout(xaxis = dict(tickformat='%d-%b\n%a', title_text = ''), yaxis = dict(title_text = '', tickmode='linear'))
 
-fig2=px.line(df, x='Date', y=['Hours Planned','Hours Worked'], title='Hours Planned/Worked',  line_shape='spline', width=300)
+fig2=px.line(df, x='Date', y=['Hours Planned','Hours Worked'], title='Hours Planned/Worked', range_y=[0,9], line_shape='spline', width=300)
 fig2.update_layout(legend_title='', xaxis = dict(tickformat='%d-%b\n%a', title_text = ''), yaxis = dict(tickmode='linear',title_text = ''))
 
 fig3=px.bar(df, x='Date', y=['GameDev','Growth','Finance','Venture'], title='Work Breakdown', width=300)
